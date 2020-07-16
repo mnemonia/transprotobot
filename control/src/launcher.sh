bd330a77f76eabe2cf47105fcd5bff6cdf369727
Mnemonia <birkeland@gmx.net>
2020-07-16 21:13:10 +0200

    Changed base directory on raspi

--- control/src/launcher.sh
+++ control/src/launcher.sh
@@ -3,6 +3,6 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
-cd home/pi/Documents/master/transprotobot/control/src
+cd home/pi/transprotobot/control/src
 sudo python3 transprotobot.py
 cd /
